e0b28492184ed4febba9a2d49c05b1444cca491d
nicvlav <32722303+nicvlav@users.noreply.github.com>
2026-02-21 21:27:21 -0800

    Implemented CalendarScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
